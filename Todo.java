8c2842602bc09b06c1dcab209f6c2550ec8480eb
Kiley Peters <kiley.m.peters-1@ou.edu>
2019-02-26 11:32:34 -0600

    First commit

---
+++ Todo.java
@@ -0,0 +1,28 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-21-21
+OU ID_______________: 113371387
+OU EMAIL____________: Kiley.m.peters-1@ou.edu
+
+Tasks:
+1.
+2. 
+3.
+4.
+5.
+
+
+Estimated Time (minutes):
+1.
+2.
+3.
+4.
+5.
+
+Actual Time (minutes):
+1.
+2.
+3.
+4.
+5.
+*/}
