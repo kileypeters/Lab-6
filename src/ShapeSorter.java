0b6b82e4569e901d591a7ca15bcfdf21568ac0e0
Kiley Peters <kiley.m.peters-1@ou.edu>
2019-02-26 19:05:37 -0600

    Completed methods

--- src/ShapeSorter.java
+++ src/ShapeSorter.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 
 /**
@@ -23,6 +24,7 @@ public class ShapeSorter
 	public ShapeSorter()
 	{
 		// TODO: complete this...
+		this.shapes = new ArrayList<Shape>();
 	}
 
 	/**
@@ -33,6 +35,7 @@ public class ShapeSorter
 	public void addShape(Shape s)
 	{
 		// TODO: complete this...
+		shapes.add(s);
 	}
 
 	/**
@@ -43,6 +46,7 @@ public class ShapeSorter
 	public void sortShapes()
 	{
 		// TODO: complete this...
+		Collections.sort(shapes);
 	}
 
 	/**
@@ -56,6 +60,7 @@ public class ShapeSorter
 	public void sortShapes(Comparator<Shape> comparator)
 	{
 		// TODO: complete this...
+		Collections.sort(shapes, comparator);
 	}
 
 	/**
@@ -68,6 +73,9 @@ public class ShapeSorter
 	public String toString()
 	{
 		// TODO: complete this...
+		
+		return shapes.toString();
+		
 	}
 
 }
