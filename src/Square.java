c48040bb979cb1fb06d5ff166fad59c5253043e0
Kiley Peters <kiley.m.peters-1@ou.edu>
2019-02-26 20:32:46 -0600

    Created methods

--- src/Square.java
+++ src/Square.java
@@ -1,13 +1,32 @@
-
+/**
+ * Lab 6
+ * 
+ * Class representing a square.
+ * 
+ * @author Kiley
+ *
+ */
 public class Square extends Rectangle{
-
+	
+	/**
+	 *  Constructor for sqaures.
+	 *  
+	 * @param id The shape's name identifier, as defined in Shape.java.
+	 * @param size The side of the square.
+	 */
 	public Square(String id, double size) 
 	{
 		super(id, size, size);
 	}
+	
+	/**
+	 * Get the type of shape.
+	 * 
+	 * @return The string "Square".
+	 */
 	public String getShapeType()
 	{
-		return "";
+		return "Square";
 	}
 
 }
