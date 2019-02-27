149e7cf0c5fd0c8e61451a9bdbf48c4bbe5afa1b
Kiley Peters <kiley.m.peters-1@ou.edu>
2019-02-26 20:33:00 -0600

    Created methods

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,24 +1,60 @@
-
+/**
+ * Lab 6
+ * 
+ * Class representing a Rectangle
+ * 
+ * @author Kiley
+ *
+ */
 public class Rectangle extends Polygon {
 
+	/**
+	 * Height of the rectangle.
+	 */
 	private double height;
 	
-	private double weight;
+	/**
+	 * Width of the rectangle
+	 */
+	private double width;
 	
+	/**
+	 * Rectangle constructor.
+	 * 
+	 * @param id The shape's name identifier, as defined in Shape.java.
+	 * @param height The height of the rectangle.
+	 * @param width The width of the rectangle.
+	 */
 	public Rectangle(String id, double height, double width) 
 	{
 		super(id);
 		
+		this.height = height;
+		this.width = width;
+		
 	}
 	
+	/**
+	 * Calculates and returns area of the rectangle.
+	 * 
+	 * @return Area of the rectangle.
+	 */
 	public double getArea()
 	{
-		return 0;
+		
+		double area = height * width;
+		
+		return area;
 	}
 	
+	/**
+	 * Gets the type of shape.
+	 * 
+	 * @return The string "Rectangle".
+	 */
 	public String getShapeType()
 	{
-		return "";
+		return "Rectangle";
 	}
 
 }
