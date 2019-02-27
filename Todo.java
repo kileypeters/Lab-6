a62bd645860d962e169f33ae688861f35798e908
Kiley Peters <kiley.m.peters-1@ou.edu>
2019-02-26 23:55:11 -0600

    Updated times

--- Todo.java
+++ Todo.java
@@ -5,24 +5,39 @@ OU ID_______________: 113371387
 OU EMAIL____________: Kiley.m.peters-1@ou.edu
 
 Tasks:
-1.
-2. 
-3.
-4.
-5.
+1. Complete Trapezoid class
+2. Complete Equilateral Triangle class
+3. Complete Polygon Class
+4. Complete Circle class
+5. Complete Shape class
+6. Complete ShapeSorter class
+7. Complete SHapeAreaComparator
+8. Complete ShapePerimetereCompatator
+9. Test Shape class
+10. Test ShapeSorter class
 
 
 Estimated Time (minutes):
-1.
-2.
-3.
-4.
-5.
+1. 60
+2. 60
+3. 60
+4. 60
+5. 60
+6. 60
+7. 60
+8. 60
+9. 60
+10. 60
 
 Actual Time (minutes):
-1.
-2.
-3.
-4.
-5.
+1. 15
+2. 20
+3. 10
+4. 20
+5. 20
+6. 20
+7. 20
+8. 20
+9. 60
+10. 60
 */}
