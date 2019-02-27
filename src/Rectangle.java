7956fa80c26bce576a3b821dc7ce684e6a0e9875
Kiley Peters <kiley.m.peters-1@ou.edu>
2019-02-26 19:43:41 -0600

    First commit

---
+++ src/Rectangle.java
@@ -0,0 +1,24 @@
+
+public class Rectangle extends Polygon {
+
+	private double height;
+	
+	private double weight;
+	
+	public Rectangle(String id, double height, double width) 
+	{
+		super(id);
+		
+	}
+	
+	public double getArea()
+	{
+		return 0;
+	}
+	
+	public String getShapeType()
+	{
+		return "";
+	}
+
+}
